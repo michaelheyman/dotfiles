c9ac29579938e8c8a7d4faf5ec54eae8b8e9643d
Michael <mitch.heyman@gmail.com>
2016-02-22 20:18:36 -0800

    updated vim setup

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 # Rename dotfiles folder
 mv ~/dotfiles ~/.dotfiles
 
+# Setup vim
+git clone git@github.com:Lokaltog/vim-distinguished.git ~/.vim/ 
+mkdir -pv ~/.vim/undo
+mkdir -pv ~/.vim/autoload ~/.vim/bundle
+curls -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -pv ~/.vim/bundle/syntastic
+git clone git@github.com:scrooloose/syntastic.git ~/.vim/bundle/syntastic
+
 # Delete bash_profile vimrc and osx
 rm ~/.bash_profile
 rm ~/.vimrc
