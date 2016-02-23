bdf504646361c4e62d30831cafea996e4f12788f
Michael <mitch.heyman@gmail.com>
2016-02-22 20:09:19 -0800

    update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,12 @@
 # Rename dotfiles folder
 mv ~/dotfiles ~/.dotfiles
 
+# Delete bash_profile vimrc and osx
+rm ~/.bash_profile
+rm ~/.vimrc
+rm ~/.osx
+
 # Create symlink between source and destination
-ln -s ~/.bash_profile ~/.dotfiles/bash_profile
-ln -s ~/.vimrc ~/.dotfiles/vimrc
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/osx ~/.osx
