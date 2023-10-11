4d17d0a048e1661d0ca5249602d83c010e33e4e2
michael <michael@Michaels-MacBook-Pro.local>
2023-10-10 21:30:47 -0700

    Install zsh and fish

--- pre-install.sh
+++ pre-install.sh
@@ -2,30 +2,62 @@
 
 set -e
 
+install_package() {
+  local package_name="$1"
+
+  if command -v apt &>/dev/null; then
+    # Install the specified package on Debian/Ubuntu
+    apt-get -qq update &> /dev/null
+    # apt-get install -qq -y "$package_name" &> /dev/null
+    apt-get install -qq -y "$package_name"
+  elif command -v brew &>/dev/null; then
+    # Install the specified package on macOS using Homebrew
+    brew install "$package_name"
+  else
+    echo "Unsupported package manager. Please install the package manually."
+    exit 1
+  fi
+}
+
 echo 'Running pre installation script'
 
-if ! [ -x "$(command -v zsh)" ]; then
-  echo 'Installing zsh'
-  ./install-zsh.sh
-fi
+install_package zsh
+install_package fish
 
 # Check if oh-my-zsh is installed
 OMZDIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OMZDIR" ]; then
   echo 'Installing oh-my-zsh'
   # Answer Y to the prompt questions "Do you want to change your default shell to zsh? [Y/n]"
-  echo "Y" | /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo "Y" | /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
   echo 'Updating oh-my-zsh'
-  upgrade_oh_my_zsh
+  omz update
 fi
 
-current_shell=$(ps -p $$ -o comm=)
+# Install oh-my-fish
+echo 'Installing oh-my-fish'
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install-fish
+chmod +x install-fish
+./install-fish --noninteractive
 
 # Change default shell
-if [ "$current_shell" != "zsh" ]; then
-  echo 'Changing default shell to zsh'
-  chsh -s $(which zsh)
+current_shell=$(ps -p $$ -o comm=)
+if [ "$(uname)" == "Darwin" ]; then
+    # Do something under Mac OS X platform        
+    if [ "$current_shell" != "fish" ]; then
+        echo 'Changing default shell to fish'
+        chsh -s $(which fish)
+    else
+        echo 'Already using fish'
+    fi
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    if [ "$current_shell" != "zsh" ]; then
+      echo 'Changing default shell to zsh'
+      chsh -s $(which zsh)
+    else
+      echo 'Already using zsh'
+    fi
 else
-  echo 'Already using zsh'
+    echo 'Unsupported platform'
 fi
