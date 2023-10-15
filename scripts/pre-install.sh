c9c59605c1830516374d742d8eddd758b309f591
michael <michael@Michaels-MacBook-Pro.local>
2023-10-14 20:41:30 -0700

    Modularize scripts and add logging

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+source ./scripts/logging.sh
+
 install_package() {
   local package_name="$1"
 
@@ -14,12 +16,12 @@ install_package() {
     # Install the specified package on macOS using Homebrew
     brew install "$package_name"
   else
-    echo "Unsupported package manager. Please install the package manually."
+    fail "Unsupported package manager. Please install the package manually."
     exit 1
   fi
 }
 
-echo 'Running pre installation script'
+info 'Running pre installation script'
 
 install_package zsh
 install_package fish
@@ -27,16 +29,16 @@ install_package fish
 # Check if oh-my-zsh is installed
 OMZDIR="$HOME/.oh-my-zsh"
 if [ ! -d "$OMZDIR" ]; then
-  echo 'Installing oh-my-zsh'
+  info 'Installing oh-my-zsh'
   # Answer Y to the prompt questions "Do you want to change your default shell to zsh? [Y/n]"
   echo "Y" | /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
-  echo 'Updating oh-my-zsh'
+  info 'Updating oh-my-zsh'
   omz update
 fi
 
 # Install oh-my-fish
-echo 'Installing oh-my-fish'
+info 'Installing oh-my-fish'
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install > install-fish
 chmod +x install-fish
 ./install-fish --noninteractive
@@ -46,18 +48,18 @@ current_shell=$(ps -p $$ -o comm=)
 if [ "$(uname)" == "Darwin" ]; then
     # Do something under Mac OS X platform        
     if [ "$current_shell" != "fish" ]; then
-        echo 'Changing default shell to fish'
+        info 'Changing default shell to fish'
         chsh -s $(which fish)
     else
         echo 'Already using fish'
     fi
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     if [ "$current_shell" != "zsh" ]; then
-      echo 'Changing default shell to zsh'
+      info 'Changing default shell to zsh'
       chsh -s $(which zsh)
     else
       echo 'Already using zsh'
     fi
 else
-    echo 'Unsupported platform'
+    fail 'Unsupported platform'
 fi
