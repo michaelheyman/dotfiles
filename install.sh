b703399492d60a99a21d26f6cc379b468ea9afc3
Michael Heyman <michaelheyman@users.noreply.github.com>
2026-05-07 10:35:11 -0700

    feat(chezmoi): Replace installation with github releases

--- install.sh
+++ install.sh
@@ -4,21 +4,18 @@
 # -u: exit on unset variables
 set -eu
 
-if ! chezmoi="$(command -v chezmoi)"; then
-	bin_dir="${HOME}/.local/bin"
-	chezmoi="${bin_dir}/chezmoi"
+bin_dir="${HOME}/.local/bin"
+chezmoi="${bin_dir}/chezmoi"
+if ! command -v chezmoi >/dev/null 2>&1 && [ ! -x "${chezmoi}" ]; then
 	echo "Installing chezmoi to '${chezmoi}'" >&2
-	if command -v curl >/dev/null; then
-		chezmoi_install_script="$(curl -fsSL https://get.chezmoi.io)"
-	elif command -v wget >/dev/null; then
-		chezmoi_install_script="$(wget -qO- https://get.chezmoi.io)"
-	else
-		echo "To install chezmoi, you must have curl or wget installed." >&2
-		exit 1
-	fi
-	sh -c "${chezmoi_install_script}" -- -b "${bin_dir}"
-	unset chezmoi_install_script bin_dir
+	mkdir -p "${bin_dir}"
+	curl -fsSL "https://github.com/twpayne/chezmoi/releases/latest/download/chezmoi-linux-amd64" \
+		-o "${chezmoi}"
+	chmod +x "${chezmoi}"
+elif command -v chezmoi >/dev/null 2>&1; then
+	chezmoi="$(command -v chezmoi)"
 fi
+unset bin_dir
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
